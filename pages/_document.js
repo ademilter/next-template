1ad957ca5050f281980e252615746eea8eb7b22e
İzni Burak Demirtaş <izniburak@outlook.com>
2020-06-25 18:47:41 +0300

    Html language moved to SiteConfig.

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang={SiteConfig.lang}>
         <Head>
           {/* base */}
           <meta charSet="utf-8" />
