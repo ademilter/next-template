533a1f4702568ee1505e6bcff68c5c42fd795502
Adem ilter <ademilter@gmail.com>
2020-06-25 15:28:10 +0300

    add ecample about page

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+import Layout from '../components/layout'
+
+function HomePage() {
+  return (
+    <Layout>
+      <Head>
+        <title>About Page</title>
+      </Head>
+
+      <h1>About page</h1>
+    </Layout>
+  )
+}
+
+export default HomePage
