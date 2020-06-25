106c3ed17611c962a698cc340abfc4b2ed25582c
Adem ilter <ademilter@gmail.com>
2020-06-25 13:40:51 +0300

    config change

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: 'Adem ilter',
-  description: 'Design and Code',
-  siteUrl: 'https://ademilter.com',
+  title: 'Next Template',
+  description: 'Next PWA Template',
+  siteUrl: 'https://github.com/ademilter/next-web-template',
   googleAnalytic: 'X-XXXXXXXXXX',
   author: {
     email: 'ademilter@gmail.com',
