5bdbbc5b21bbe7a7cc3d2ba1443e700109bcffe7
Adem ilter <ademilter@gmail.com>
2020-06-25 13:40:33 +0300

    store refactor, clear app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import StoreContext from '../store'
-import { THEME } from '../constants'
+import React from 'react'
+import { StoreProvider } from '../store'
 
 import '../styles/app.css'
 
 export default function MyApp({ Component, pageProps }) {
-  const [theme, themeSet] = useState(null)
-
-  useEffect(() => {
-    const theme = localStorage.getItem('THEME') || THEME.LIGHT
-    themeSet(theme)
-  }, [])
-
-  const changeTheme = (theme) => {
-    themeSet(theme)
-    localStorage.setItem('THEME', theme)
-  }
-
-  useEffect(() => {
-    if (!theme) return
-    const $html = document.querySelector('html')
-    $html.classList.remove(...$html.classList)
-    $html.classList.add(theme.toString())
-  }, [theme])
-
   return (
-    <StoreContext.Provider value={{ theme, changeTheme }}>
+    <StoreProvider>
       <Component {...pageProps} />
-    </StoreContext.Provider>
+    </StoreProvider>
   )
 }
--- store/index.js
+++ store/index.js
@@ -1,5 +1,33 @@
-import { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import { THEME } from '../constants'
 
 const StoreContext = createContext(null)
+
+export function StoreProvider({ children }) {
+  const [theme, themeSet] = useState(null)
+
+  useEffect(() => {
+    const theme = localStorage.getItem('THEME') || THEME.LIGHT
+    themeSet(theme)
+  }, [])
+
+  const changeTheme = (theme) => {
+    themeSet(theme)
+    localStorage.setItem('THEME', theme)
+  }
+
+  useEffect(() => {
+    if (!theme) return
+    const $html = document.querySelector('html')
+    $html.classList.remove(...$html.classList)
+    $html.classList.add(theme.toString())
+  }, [theme])
+
+  return (
+    <StoreContext.Provider value={{ theme, changeTheme }}>
+      {children}
+    </StoreContext.Provider>
+  )
+}
 
 export default StoreContext
