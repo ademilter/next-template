ca68ef9b55f41aa331bfa85b272511ef046668d0
Adem ilter <ademilter@gmail.com>
2020-12-25 23:28:11 +0300

    next fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { StoreProvider } from '../store'
 
 import '../styles/app.css'
@@ -6,6 +7,9 @@ import '../styles/app.css'
 export default function MyApp({ Component, pageProps }) {
   return (
     <StoreProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </StoreProvider>
   )
